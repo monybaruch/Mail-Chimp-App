f5f1e3b99f75fab3f125f0a1e0a40f8aa5f15e45
מוני ברוך <monybaruch@gmail.com>
2023-03-15 18:43:52 +0200

    finish setting up server

--- app.js
+++ app.js
@@ -2,12 +2,32 @@ const express = require("express");
 const https = require("https");
 const bodyParser = require("body-parser");
 const app = express();
+
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/signup.html");
 });
 
+app.post("/", function (req, res) {
+  const lastName = req.body.lastName;
+  const firstName = req.body.firstName;
+  const email = req.body.email;
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        },
+      },
+    ],
+  };
+  const jsonData = JSON.stringify(data);
+});
 app.listen(3000, function () {
   console.log("server started on port 3000");
 });
